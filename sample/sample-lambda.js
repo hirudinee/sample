cc68a61042e7577e70d4e979aed7f4df81245424
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-11 18:31:57 +0530

    initail commit

--- sample/sample-lambda.js
+++ sample/sample-lambda.js
@@ -1,6 +1,17 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
-    console.log("Lambda started");
+    ddb.put({
+        TableName: 'hiruS3Sample123',
+        Item: {}
+    }, function (err, data) {
+        if (err) {
+            //handle error
+        } else {
+            //your logic goes here
+        }
+    });
+
 
 }
